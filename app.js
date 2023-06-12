553223c410e876ff4504c86ce161c38a6583c01f
Nico <jndr051701@gmail.com>
2023-06-12 15:18:58 -0600

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList.add('modal__overlay--visible');
+
+    emailjs
+    .sendForm(
+        'service_ngkzynp',
+        'template_5phd2lf',
+        event.target,
+        'h8ooOzyl9xkdyyQ2N'
+    ).then(()=> {
+        loading.classList.remove('modal__overlay--visible');
+        success.classList.add('modal__overlay--visible');
+    }).catch (() => { 
+        loading.classList.remove('modal__overlay--visible');
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at nico051701@outlook.com"
+        )
+    })
+}
+
+
