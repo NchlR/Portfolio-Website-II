6cf6cd542b3ed9308dece7a30eec2499cef94536
Nico <jndr051701@gmail.com>
2023-06-14 14:29:52 -0600

    added dark-theme

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+    document.body.classList.add("dark-theme")
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+
+
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
@@ -22,6 +37,11 @@ function contact(event) {
 }
 
 function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
     document.body.classList.add("modal--open");
 }
 
