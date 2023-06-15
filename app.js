94d1b8fab2407cc7d7c6e12f256cbe5b2bba4e4d
Nico <jndr051701@gmail.com>
2023-06-14 21:40:54 -0600

    complete

--- app.js
+++ app.js
@@ -1,48 +1,59 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
-function toggleContrast() {
-    contrastToggle = !contrastToggle
-    if (contrastToggle) {
-    document.body.classList.add("dark-theme")
-    }
-    else {
-        document.body.classList.remove("dark-theme")
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
     }
 }
 
 
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList.add("dark-theme");
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
-    event.preventDefault();
-    const loading = document.querySelector('.modal__overlay--loading');
-    const success = document.querySelector('.modal__overlay--success');
-    loading.classList.add('modal__overlay--visible');
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList.add("modal__overlay--visible");
 
-    emailjs
+  emailjs
     .sendForm(
-        'service_ngkzynp',
-        'template_5phd2lf',
-        event.target,
-        'h8ooOzyl9xkdyyQ2N'
-    ).then(()=> {
-        loading.classList.remove('modal__overlay--visible');
-        success.classList.add('modal__overlay--visible');
-    }).catch (() => { 
-        loading.classList.remove('modal__overlay--visible');
-        alert(
-            "The email service is temporarily unavailable. Please contact me directly at nico051701@outlook.com"
-        )
+      "service_ngkzynp",
+      "template_5phd2lf",
+      event.target,
+      "h8ooOzyl9xkdyyQ2N"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList.add("modal__overlay--visible");
     })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at nico051701@outlook.com"
+      );
+    });
 }
 
 function toggleModal() {
-    if (isModalOpen) {
-        isModalOpen = false;
-        return document.body.classList.remove("modal--open");
-    }
-    isModalOpen = true;
-    document.body.classList.add("modal--open");
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList.add("modal--open");
 }
-
-
